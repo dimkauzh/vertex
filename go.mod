d8975dcabbe92d19da724d565d1a637dfdf9a78c
Dima Marhitych <uzhdimka@gmail.com>
2023-09-17 22:23:03 +0200

    Setup of opengl

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dimkauzh/vertex-engine
 
 go 1.14
 
-require github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b
+require (
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b
+)
--- src/vertex/window/window.go
+++ src/vertex/window/window.go
@@ -1,6 +1,9 @@
 package window
 
 import (
+	"fmt"
+
+	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
@@ -8,11 +11,18 @@ func NewWindow(width int, height int, title string) *glfw.Window {
 	window, err := glfw.CreateWindow(width, height, title, nil, nil)
 
 	if err != nil {
-		print("Error create window")
+		fmt.Println("Error create window")
 		panic(err)
 	}
 
 	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		fmt.Println("Error init gl")
+
+		panic(err)
+
+	}
 
 	return window
 }
