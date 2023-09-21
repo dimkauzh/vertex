e5c10433d9738093f4effb7c5d0326cd4e581a56
Dima Marhitych <uzhdimka@gmail.com>
2023-09-21 21:52:42 +0200

    Moved image system to other file

--- examples/example1.go
+++ examples/example1.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/dimkauzh/vertex-engine/src/vertex"
-	"github.com/dimkauzh/vertex-engine/src/vertex/draw"
+	"github.com/dimkauzh/vertex-engine/src/vertex/image"
 	w "github.com/dimkauzh/vertex-engine/src/vertex/window"
 )
 
@@ -13,13 +13,13 @@ func main() {
 	window := w.NewWindow(800, 600, "Vertex Engine")
 
 	imagePath := "examples/mushroom.png"
-	image := draw.LoadImage(imagePath, 1, 1, 20, 600) //550, 550, 20, 20)
+	mushroom := image.LoadImage(imagePath, 1, 1, 20, 600) //550, 550, 20, 20)
 
 	for w.Loop(window) {
 		//draw.SetBackgroundColor([3]float32{0.2, 0.2, 0.2})
 		// draw.DrawLine(-0.5, -0.5, 0.5, 0.5, [3]float32{1, 0, 0})
 		// draw.DrawRect(-0.5, 0.2, 0.5, 0.5, [3]float32{0, 1, 1})
-		draw.DrawImage(image)
+		image.DrawImage(mushroom)
 		w.Refresh(window)
 	}
 }
